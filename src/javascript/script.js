0f09bb424f114a0ea52dbdaf8ccaa77cf7e30713
sophxs <115382818+sophxs@users.noreply.github.com>
2024-07-07 21:50:10 -0300

    Add files via upload

---
+++ src/javascript/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+ ScrollReveal().reveal('.icon-content', {
+  origin: 'left',
+  duration: 2000,
+  distance: '100%'
+ });
+
+ ScrollReveal().reveal('#about_me', {
+  origin: 'right',
+  duration: 2000,
+  distance: '50%'
+ });
+
+ ScrollReveal().reveal('#links', {
+  origin: 'left',
+  duration: 2000,
+  distance: '50%'
+ });
+
+ ScrollReveal().reveal('#social_media', {
+  origin: 'right',
+  duration: 2000,
+  distance: '50%'
+ });
+});
